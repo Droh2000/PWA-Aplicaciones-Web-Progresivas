d830dada47624547f91a6b21e947bffe5ad59ee0
gomezfarm <gomez.farm@outlook.com>
2025-04-02 08:46:45 -0600

    Formas de hacer peticiones desde el Fetch

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -15,10 +15,23 @@
 self.addEventListener('fetch', event => {
     // Aqui veremos las peticiones que hace el navegador a los archivos del proyecto y archivos importados como los CDN de bootstrap
     //      console.log(event);
+    // Estas son las peticiones que el Fetch esta haciendo en el backgorund, todas estas peticiones son las que es registran en la pestana de Network
+    // De aqui podemos sacar muchas propiedades como la URL especifica, tambien vemos en la propiedad "mode" que unas son de tipo "cors" o "no-cors"
+    //      * Las peticiones de tipo CORS permite solicitar el recurso de otro dominio en esta aplicacion
+    //      * Las peticiones de tipo No-CORS son recursos que corresponden a la misma aplicacion
+    //  console.log(event.request);
+    //  event.request.url -> Obtenemos la ruta exacta donde se encuentra el recurso
 
     // Bloquear la peticion a un recurso del proyecto (Para que el SW no lo use)
     if( event.request.url.includes('style.css') ){
         // Simulamos un 404 (En la pestana Network veremos que nos da error)
+
+        // Podemos obtener el recurso de otra parte y mandarsela para que lo ponga en el recurso que tomamos del IF
+        //  const styleFile = fetch('./css/style.css');
+        //  const styleReq = fetch( event.request.url ); // Aqui nos regesa el mismo archivo
+        //  const styleReq = fetch( event.request ); // Esta es otra forma de hacer lo mismo de arriba
+
+        //  event.respondWith( styleReq );
         event.respondWith( null );
     }else{
         // Aqui le decimos que la respuesta del nuevo evento sera lo que venga al hacer la peticion, es como recibir los datos de la solicitud
