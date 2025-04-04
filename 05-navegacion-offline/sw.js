e13bb35ed1720848c0317659855766a1d6ed77bc
gomezfarm <gomez.farm@outlook.com>
2025-04-04 09:29:55 -0600

    Borrando versiones vieja del cache automaticamente

--- 05-navegacion-offline/sw.js
+++ 05-navegacion-offline/sw.js
@@ -1,5 +1,7 @@
 // Aqui vamos a ver que de las estrategias del cache podemos extenderlas en sus funcionalidades segun nuestras necesidades
-const CACHE_STATIC_NAME  = 'static-v1';
+// Cuando levantamos la version del cache (Por ejemplo pasa de v1 a v2) porque hay nuevo cambios, hay que hacer que cuando 
+// se recarga el navegador web, borre la version del cache que ya no nos sirve (Para no hacerlo manualmente desde el navegador)
+const CACHE_STATIC_NAME  = 'static-v2'; // Al cambiar y recargar el navegador web veremos en Cache Storage que se instala y se mantiene la version vieja del cache
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 const CACHE_DYNAMIC_LIMIT = 50;
@@ -36,6 +38,30 @@ self.addEventListener('install', e => {
             .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
 
     e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
+});
+
+// Recordemos que cuando se hace la instalacion se crea ese cache, la clave esta en borrar los caches cuando el nuevo SW
+// toma el control de la aplicacion
+self.addEventListener('activate', e => {
+    // Esto solo se va a disparar cuando la instalacion termine
+    // Debemos de borrar todos los caches en los que su Nombre no coincide con el que tenemos especificados en las constantes de arriba
+    // Pero nosotros debemos especificar los nombres, en este caso el que cambio fue el de Static asi que ese buscamos
+    const respCache = caches.keys().then( keys => {
+        // Aqui obtenemos los nombres asi que los recorremos
+        keys.forEach( key => {
+            // Si es diferente al de Static e incluye ese nombre es lo que borramos
+            if( key !== CACHE_STATIC_NAME && key.includes('static') ){
+                return caches.delete(key);
+            }
+        });
+    });
+
+    // Esperamos a que esta tarea termine para no pasar al siguiente Listener
+    e.waitUntil(respCache);
+    // Hay que recargar el navegador y precionamos SkipWaiting para que se instalen los caches y sus nuevas versiones
+    // Si subimos de version otravez, veremos que lo luego se agrega este nuvo cache pero no se activara esta funcion hasta que
+    // no se precione SkipWaiting, esto es un mecanismo de seguridad por si el cliente se queda sin conexion, este puede seguir
+    // navegando porque su version del Cache aunque sea vieja le permite hacer eso, ya cuando vuelva a tener conexion se le borrara el cache viejo
 });
 
 // Estrategia: Cache With Network Fallback
