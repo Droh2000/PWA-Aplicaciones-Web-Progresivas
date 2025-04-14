84b17a4285b716a51246829872a7f88cb04cff46
gomezfarm <gomez.farm@outlook.com>
2025-04-14 14:44:03 -0600

    Servicios REST necesarios para PUSH, Subscribe, Key

--- 10-twittor-con-push/server/routes.js
+++ 10-twittor-con-push/server/routes.js
@@ -40,6 +40,23 @@ router.post('/', function (req, res) {
   });
 });
 
+// Configuracion de las rutas nesesarias
 
+// Para almacenar la subscricion (Aqui manejamos cuando el cliente se subscriba)
+router.post('/subscribe', (req, res) => {
+  res.json('subscribe');
+});
+
+// Para obtener la llave publica
+router.get('/key', (req, res) => {
+  res.json('key publico');
+});
+
+// Para Enviar una notificacion PUSH a las personas que nosotros queramos
+// Normalmente esto no es mediante un servicio REST ya que es algo que se controla del lado del Server
+// No es un servicio que este expuesto sino solo algo que corra en el backend pero para fines de aprendisaje lo colocamos
+router.post('/push', (req, res) => {
+  res.json('');
+});
 
 module.exports = router;
