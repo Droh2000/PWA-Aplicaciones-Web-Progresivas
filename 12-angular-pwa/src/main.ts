e7feb8a4414c5cce1ae28e8c2c657fca608ad14a
gomezfarm <gomez.farm@outlook.com>
2025-04-17 12:38:07 -0600

    ng add @angular/pwa

--- 12-angular-pwa/src/main.ts
+++ 12-angular-pwa/src/main.ts
@@ -2,7 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 }).catch((err) => console.error(err));
